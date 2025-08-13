d7604ff11d0bd1c308503bc1da8452b5a2564a17
Juliotbr <juliojulioster@gmail.com>
2025-08-13 13:52:53 -0300

    adicionando FooterSistema

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Footer from "./components/footer/Footer"
+import FooterSistema from "./components/footer/FooterSistema"
 
 function App() {
   return (
     <>
     <Footer/>
+    <FooterSistema/>
     </>    
   )
 }
