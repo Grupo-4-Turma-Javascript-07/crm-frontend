bf05a630ed20e939fece33402b171004ee5c2565
Melissa <melissasilva245@gmail.com>
2025-08-13 14:09:59 -0300

    implementação da pagina de login e de cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import Login from "./components/login/Login"
+import Cadastro from "./components/cadastro/Cadastro";
 
 function App() {
   return (
-    <Login />
-  )
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App
--- src/components/cadastro/Cadastro.tsx
+++ src/components/cadastro/Cadastro.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import logo from "../../assets/img/CRM4ULOGO.png"
 
-const Login: React.FC = () => {
+const Cadastro: React.FC = () => {
   const handleLoginClick = () => {
-    window.location.href = "/cadastro";
+    window.location.href = "/login";
   };
 
   return (
@@ -18,10 +18,10 @@ const Login: React.FC = () => {
         <div className="absolute bg-black opacity-60 inset-0 z-0"></div>
         <div className="w-full px-24 z-10">
           <h1 className="text-4xl font-bold text-left tracking-wide">
-            Bem-vindo de volta!
+            Junte-se a nós!
           </h1>
           <p className="text-3xl my-4">
-            Conecte-se com a gente.
+            Crie sua conta e comece sua jornada.
           </p>
         </div>
       </div>
@@ -42,10 +42,21 @@ const Login: React.FC = () => {
           <h1 className="my-6 flex justify-center items-center">
             <img src={logo} alt="Logo" className="h-28 w-auto" />
           </h1>
-          <div className="py-6 space-x-2">
-            <button onClick={handleLoginClick} className="cursor-pointer hover:underline hover:text-roxo-100">Clique aqui para se cadastrar!</button>
+          <div className="py-6 space-x-2 hover:">
+            <button onClick={handleLoginClick}  className="cursor-pointer hover:underline hover:text-roxo-100">
+              Já tem conta? Clique aqui para entrar!
+            </button>
           </div>
           <form className="sm:w-2/3 w-full px-4 lg:px-0 mx-auto">
+            <div className="pb-2 pt-4">
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Nome completo"
+                className="block w-full p-4 text-lg rounded-sm bg-black"
+              />
+            </div>
             <div className="pb-2 pt-4">
               <input
                 type="email"
@@ -64,12 +75,22 @@ const Login: React.FC = () => {
                 className="block w-full p-4 text-lg rounded-sm bg-black"
               />
             </div>
-            <div className="text-right text-gray-400 hover:underline hover:text-gray-100">
-              <a href="#">Esqueceu a senha?</a>
+            <div className="pb-2 pt-4">
+              <input
+                type="password"
+                name="confirmPassword"
+                id="confirmPassword"
+                placeholder="Confirmar senha"
+                className="block w-full p-4 text-lg rounded-sm bg-black"
+              />
+            </div>
+            <div className="pb-2 pt-4">
             </div>
             <div className="px-4 pb-2 pt-4">
-              <button className="flex-1 inline-flex justify-center items-center py-3 px-6 border border-transparent shadow-lg text-lg font-bold rounded-lg text-branco bg-gradient-to-r from-roxo-100 to-rosa-100 hover:from-roxo-50 hover:to-rosa-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-roxo-100 transition-all duration-300 transform hover:scale-105">
-                Entrar
+              <button 
+                type="submit"
+                className="w-full inline-flex justify-center items-center py-3 px-6 border border-transparent shadow-lg text-lg font-bold rounded-lg text-branco bg-gradient-to-r from-roxo-100 to-rosa-100 hover:from-roxo-50 hover:to-rosa-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-roxo-100 transition-all duration-300 transform hover:scale-105">
+                Criar conta
               </button>
             </div>
           </form>
@@ -79,4 +100,4 @@ const Login: React.FC = () => {
   );
 };
 
-export default Login;
+export default Cadastro;
